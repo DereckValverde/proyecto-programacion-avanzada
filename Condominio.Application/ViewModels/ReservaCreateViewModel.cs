577fcee569748a7e08cd37f0d726c77eaa5fc3be
Dereck Valverde <derecktiti1234@gmail.com>
2026-08-14 12:16:07 -0600

    Arreglo doble verificacion

--- Condominio.Application/ViewModels/ReservaCreateViewModel.cs
+++ Condominio.Application/ViewModels/ReservaCreateViewModel.cs
@@ -8,22 +8,17 @@ namespace Condominio.Application.ViewModels.Reserva
 {
     public class ReservaCreateViewModel
     {
-        [Required]
         [DataType(DataType.Date)]
         public DateTime FechaReserva { get; set; }
 
-        [Required]
         public TimeSpan HoraInicio { get; set; }
 
-        [Required]
         public TimeSpan HoraFin { get; set; }
 
         public EstadoReserva Estado { get; set; }
 
-        [Required]
         public int IdVivienda { get; set; }
 
-        [Required]
         public int IdArea { get; set; }
 
         public IEnumerable<SelectListItem> Viviendas { get; set; }
